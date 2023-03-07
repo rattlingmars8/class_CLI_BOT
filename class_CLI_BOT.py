873d8c7b407ddae1364f8949137d1caeabc10e68
rattlingmars8 <rattlingmars8@gmail.com>
2023-03-07 20:16:08 +0100

    Update class_CLI_BOT.py

--- class_CLI_BOT.py
+++ class_CLI_BOT.py
@@ -2,68 +2,72 @@ from collections import UserDict
 import re
 
 
-def normal_phone(phone: str):
-    digits = [char for char in phone if char.isdigit()]
-    if len(digits) == 10:
-        return "+38" + "".join(digits)
-    elif len(digits) == 12 and digits[:3] == ["3", "8", "0"]:
-        return "+" + "".join(digits)
-    elif len(digits) == 12 and phone[0] == "+" and digits[:3] == ["3", "8", "0"]:
-        return "+" + "".join(digits)
-    else:
-        raise ValueError("Invalid phone number")
-
-
 class Field:
     def __init__(self, value):
         self.value = value
 
-    def __repr__(self):
-        return str(self.value)
-
+    def __str__(self) -> str:
+        return f'{self.value}'
 
 class Name(Field):
     pass
 
-
 class Phone(Field):
-        def __init__(self, value):
-            self.value = normal_phone(value)
+    def __init__(self, value):
+        self._value = self.normal_phone(value)
 
-        @property
-        def value(self):
-            return self._value
+    def normal_phone(self, phone: str):
+        digits = [char for char in phone if char.isdigit()]
+        if len(digits) == 10:
+            return "+38" + "".join(digits)
+        elif len(digits) == 12 and digits[:3] == ["3", "8", "0"]:
+            return "+" + "".join(digits)
+        elif len(digits) == 12 and phone[0] == "+" and digits[:3] == ["3", "8", "0"]:
+            return "+" + "".join(digits)
+        else:
+            raise ValueError("Invalid phone number")
 
-        @value.setter
-        def value(self, value):
-            self._value = normal_phone(value)
+    @property
+    def value(self):
+        return self._value
 
-    
+    @value.setter
+    def value(self, value):
+        self._value = self.normal_phone(value)
+
+    def __str__(self):
+        return self._value
 
 class Record():
-    def __init__(self, name:Name, phone:Phone = None):
-        self.name = name 
+    def __init__(self, name:Name, phone:Phone=None):
+        self.name = name
         self.phones = [phone] if phone else []
+
+    def add_phone(self, phone:Phone):
+        self.phones.append(phone)
+
+    def remove_phone(self, phone:Phone):
+        self.phones.remove(phone)
+
+    def change_phone(self, index: int, new_phone:Phone):
+        self.phones[index] = new_phone
+
+    def __str__(self):
+        phones = "\n".join([str(phone) for phone in self.phones])
+        return f"{self.name}: {phones}"
     
-    def add_phone(self, other_phone:Phone):
-            self.phones.append(other_phone)
 
-    def del_phone(self, phone:Phone):
-        for i, p in enumerate(self.phones):
-            if p.value == phone.value:
-                return self.phones.pop(i)
-            
-    def change_phone(self, old_phone:Phone, new_phone:Phone):
-        if self.del_phone(old_phone):
-            self.add_phone(new_phone)
-
-    def __repr__(self) -> str:
-      return f'{self.phones}'
-        
-        
 class AddressBook(UserDict):
     def add_record(self, record:Record):
-        self.data[record.name.value] = record.phones
+        self.data[record.name.value] = record
+    
+# name = Name("Vladik")
+# phone = Phone('0930030322')
+# rec = Record(name, phone)
+# ab = AddressBook()
+# ab.add_record(rec)
+# print(ab)    
+
 
 
 contacts = AddressBook()
@@ -72,9 +76,7 @@ contacts = AddressBook()
 def input_error(func):
     def wrapper(*args, **kwargs):
         try:
-            return func(
-                *args, **kwargs
-            )  # Call the decorated function with the given arguments
+            return func(*args, **kwargs)  # Call the decorated function with the given arguments
         except KeyError:
             return "This contact doesn't exist in the phonebook"
         except ValueError:
@@ -89,73 +91,85 @@ def input_error(func):
 def hello(*args):
     return "How can I help you?"
 
+
 # fnc to add a new contact to the phonebook
-@input_error
-def add_contact(*args, **kwargs):
+def add_contact(*args):
     name = Name(args[0])
     if name.value in contacts.keys():
-        return "This contact already exists"
+        return "This user already exists."
     else:
         try:
             phone = Phone(args[1])
         except IndexError:
-            phone = None
+            phone = ""
         record = Record(name, phone)
         contacts.add_record(record)
-        return f"{name} with {phone} has been added to the phonebook"
+        if phone:
+            return f"{name} with {phone} has been added to the phonebook"
+        return f"{name} has been added to the phonebook"
 
-#fnc to append phone number to phones of some contact/////command should looks like: append phone (name) (new phone)  
+def show_all(*args):
+    all_contacts = ""
+    for name, record in contacts.items():
+        all_contacts += f'{record}\n'
+    return all_contacts
+
+# def change_phone(*args):
+#     record = contacts.get(args[0])
+#     for i, number in enumerate([phone.value for phone in record.phones], 0): 
+#             print(f'{i}: {number}')
+#     while True: 
+#         try:        
+#             pos = int(input('Enter the index of a phone you want to edit >>> '))
+#             if pos > len(record.phones)-1:
+#                 raise IndexError
+#         except IndexError:
+#             return pos = int(input('Wrong index. Enter the index of a phone you want to edit >>> '))
+#         except ValueError:
+#             return pos = int(input('Wrong index. Enter the index of a phone you want to edit >>> '))
+#         user_input = input('Enter new phone num for this contact >>>> ')
+#         try:
+#             new_phone = Phone(user_input)
+#             break
+#         except ValueError:
+#             return "Enter valid phone"
+#     record.change_phone(pos, new_phone)
+#     return f"Phone was changed on {new_phone}"
 @input_error
-def append_phone(*args):
-    name = args[0]
-    phone = args[1]
-    if name not in contacts:
-        return "This contact doesn't exist in the phonebook"
-    else:
-        record = Record(Name(name), Phone(phone))
-        contacts[name].append(record.phones[0])
-        return f"{phone} has been added to {name}'s phones"
-
-#fnc to change contact phone num by typing old phone num 
-@input_error
-def change_contact(*args, **kwargs):
-    name = Name(args[0])
-    if name.value in contacts.keys():
-        old_phone = Phone(args[1])
-        if old_phone.value in [phone.value for phone in contacts[name.value]]:
-            new_phone = Phone(args[2])
-            record = Record(name, old_phone)
-            for i, phone in enumerate(contacts[name.value]):
-                if phone.value == old_phone.value:
-                    contacts[name.value][i] = new_phone
-                    record.change_phone(old_phone, new_phone)
-                    return f"The phone number for {name} has been updated to {new_phone}"
-    else:
-        return "This contact doesn't exist"
-
-# fnc to find the phone number of a given contact
-# @input_error
-# def find_phone(*args, **kwargs):
-#     name = args[0]
-#     return f"The phone number for {name} is {contacts[name]}"
+def change_phone(*args):
+    record = contacts.get(args[0])
+    for i, number in enumerate([phone.value for phone in record.phones], 0):
+            print(f'{i}: {number}')
+    while True:
+        try:
+            pos = int(input('Enter the index of a phone you want to edit >>> '))
+            if pos > len(record.phones)-1:
+                raise IndexError
+        except IndexError:
+            print('Wrong index. Please try again.')
+        except ValueError:
+            print('Please enter a valid integer index.')
+        else:
+            user_input = input('Enter new phone num for this contact >>>> ')
+            try:
+                new_phone = Phone(user_input)
+            except ValueError:
+                print('Please enter a valid phone number.')
+            else:
+                record.change_phone(pos, new_phone)
+                return f"Phone was changed to {new_phone}."
 
 
-# fnc to show all contacts and their phone numbers
-@input_error
-def show_all(*args, **kwargs):
-    output = ""
-    for name, phone in contacts.items():
-        output += f"{name}: {phone}\n"
-    return output
+
+    
 
 
 COMMANDS = {
     hello: 'hello',
     show_all: "show all",
     add_contact: "add",
-    change_contact: "change",
+    change_phone: "change phone", 
     # find_phone: "phone",
-    append_phone: 'append phone'
 }
 
 
